60686d2f9a13301ebe1adabcde1c1fe7de9ed7f6
wxy325 <wxy325@qq.com>
2014-03-04 15:47:27 +0800

    Initial Commit

---
+++ Going/WXYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WXYAppDelegate.h
+//  Going
+//
+//  Created by wxy325 on 3/4/14.
+//  Copyright (c) 2014 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Going/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Going
+//
+//  Created by wxy325 on 3/4/14.
+//  Copyright (c) 2014 wxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WXYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WXYAppDelegate class]));
+    }
+}
